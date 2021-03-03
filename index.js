3a9f9912396164eee4f468b82857b18564475fa4
thipages <thipages@gmail.com>
2021-03-03 01:15:38 +0100

    add rollup config, min.js, index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var index = (source,callback) => {
+    const t=typeof source==='function'?source():source;
+    const f=t instanceof File ? t :t.files[0];
+    const b =(h,d,e=false)=> ({
+        header: h,
+        body: d,
+        error:e,
+        file:f,
+        all: function () {
+            return this.header + this.body
+        }
+    });
+    const reader = new FileReader();
+    reader.onload=()=> {
+        const rr = reader.result, index = rr.indexOf(',');
+        if (index===-1) callback('',rr);
+        else callback(b(rr.slice(0, index + 1),rr.slice(index + 1)));
+    };
+    reader.onerror=()=>callback('','',true);
+    reader.readAsDataURL(f);
+};
+
+export default index;
